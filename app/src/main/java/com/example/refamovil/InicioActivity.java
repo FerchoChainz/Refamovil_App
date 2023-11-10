553d95ecc119ea3c329d8d939f0f56481b5b0a09
Rodrigo Ramos <ramoslozanorodrigoalejandro@gmail.com>
2023-11-10 12:13:30 -0600

    Finish activity Home and activity Products

--- app/src/main/java/com/example/refamovil/InicioActivity.java
+++ app/src/main/java/com/example/refamovil/InicioActivity.java
@@ -13,7 +13,11 @@ import androidx.appcompat.widget.Toolbar;
 
 
 import com.example.refamovil.fragments.BuscarProductosFragment;
+import com.example.refamovil.fragments.HomeFragment;
 import com.example.refamovil.fragments.ProductosFragment;
+import com.example.refamovil.fragments.SobreNosotrosFragment;
+import com.example.refamovil.fragments.SucursalesFragment;
+import com.example.refamovil.fragments.VerComprasFragment;
 import com.google.android.material.navigation.NavigationView;
 
 public class InicioActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
@@ -37,19 +41,26 @@ public class InicioActivity extends AppCompatActivity implements NavigationView.
         toggle.syncState();
 
         if (savedInstanceState == null){
-            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ProductosFragment()).commit();
-            navigationView.setCheckedItem(R.id.nav_productos);
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_Inicio);
         }
     }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        if (item.getItemId() == R.id.nav_productos){
+        if (item.getItemId() == R.id.nav_Inicio){
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
+        } else if (item.getItemId() == R.id.nav_Productos) {
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ProductosFragment()).commit();
-        } else if (item.getItemId() == R.id.pruebaId) {
+        }else if (item.getItemId() == R.id.nav_Buscar_Productos) {
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new BuscarProductosFragment()).commit();
+         }else if (item.getItemId() == R.id.nav_Compras) {
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new VerComprasFragment()).commit();
+     }else if (item.getItemId() == R.id.nav_sucursales) {
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new SucursalesFragment()).commit();
+    }else if (item.getItemId() == R.id.nav_nosotros) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new SobreNosotrosFragment()).commit();
         }
-
         drawerLayout.closeDrawer(GravityCompat.START);
         return true;
     }
--- app/src/main/java/com/example/refamovil/fragments/ProductosFragment.java
+++ app/src/main/java/com/example/refamovil/fragments/ProductosFragment.java
@@ -7,8 +7,14 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.example.refamovil.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,12 +23,8 @@ import com.example.refamovil.R;
  */
 public class ProductosFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
@@ -30,15 +32,6 @@ public class ProductosFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ProductosFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static ProductosFragment newInstance(String param1, String param2) {
         ProductosFragment fragment = new ProductosFragment();
         Bundle args = new Bundle();
@@ -60,7 +53,37 @@ public class ProductosFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_productos, container, false);
+        View root = inflater.inflate(R.layout.fragment_productos, container, false);
+
+        // Obtén una referencia al Spinner desde el diseño XML
+        Spinner spinner = root.findViewById(R.id.spTipo);  // Asegúrate de tener el ID correcto
+
+        // Crear una lista de opciones para el Spinner
+        List<String> opciones = new ArrayList<>();
+        opciones.add("Aceites");
+        opciones.add("Autopartes");
+
+        // Crear un adaptador
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(requireContext(), android.R.layout.simple_spinner_item, opciones);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // Asignar el adaptador al Spinner
+        spinner.setAdapter(adapter);
+
+        // Manejar la selección del Spinner
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String opcionSeleccionada = opciones.get(position);
+                // Realizar acciones con la opción seleccionada
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // Manejar la selección nula si es necesario
+            }
+        });
+
+        return root;
     }
 }
